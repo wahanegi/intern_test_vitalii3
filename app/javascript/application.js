aa141a2ad7b5e0f1ebe2096ba4cd1a37d8f21f4c
VitaliiShevchenko <iksel@ukr.net>
2023-01-31 11:48:22 +0200

    TRY #12 without importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "../assets/javascript/application"
+// import "../assets/javascript/application"
 // import "controllers"
 // import "trix"
 // import "@rails/actiontext"
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,9 +3,9 @@
 # pin "jquery", to: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.js"
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-# pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-# pin_all_from "app/javascript/controllers", under: "controllers"
-# pin "trix"
-# pin "@rails/actiontext", to: "actiontext.js"
-# pin "cut_image"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
+pin "cut_image"
