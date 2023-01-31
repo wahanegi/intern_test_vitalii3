2ea4ba2ae2e677961580965500a4be055af03448
VitaliiShevchenko <iksel@ukr.net>
2023-01-31 11:01:02 +0200

    TRY #10 with importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "jquery", to: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.js"
+# pin "jquery", to: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.js"
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.js"
-pin "cut_image"
+# pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+# pin_all_from "app/javascript/controllers", under: "controllers"
+# pin "trix"
+# pin "@rails/actiontext", to: "actiontext.js"
+# pin "cut_image"
